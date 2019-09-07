b0b946c3a1b7d5aceb7b4b58657210ca23911ea7
Daniel Carlson <dancarl758@gmail.com>
2019-09-07 13:45:50 +0100

    Successfully gotten issues based on typed in organization and repository

--- src/App.js
+++ src/App.js
@@ -8,37 +8,63 @@ const axiosGithubGraphQL = axios.create({
   }
 })
 
-const GET_ORGANIZATION = `
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_ISSUES_OF_REPOSITORY = `
+  query($organization: String!, $repository: String!) {
+    organization(login: $organization) {
       name
       url
+      repository(name: $repository) {
+        name
+        url
+        issues(last: 5) {
+          edges {
+            node {
+              id
+              title
+              url
+            }
+          }
+        }
+      }
     }
   }
 `
+
+const getIssuesOfRepository = (path) => {
+  const [organization, repository] = path.split('/')
+
+  return axiosGithubGraphQL.post('', {
+    query: GET_ISSUES_OF_REPOSITORY,
+    variables: { organization, repository}
+  })
+}
 
 const TITLE = "React GraphQL Github Client"
 
 const App = () => {
 
   const [path, setPath] = useState('the-road-to-learn-react/the-road-to-learn-react')
+  const [organization, setOrganization] = useState(null)
+  const [errors, setErrors] = useState(null)
 
   useEffect(() => {
-    onFetchFromGithub()
-  }, [])
+    onFetchFromGithub(path)
+  }, [path])
 
   const onChange = (event) => {
     setPath(event.target.value)
   }
 
   const onSubmit = (event) => {
+    onFetchFromGithub(path)
     event.preventDefault()
   }
 
-  const onFetchFromGithub = () => {
-    axiosGithubGraphQL
-      .post('', { query: GET_ORGANIZATION })
-      .then(result => console.log(result))
+  const onFetchFromGithub = (path) => {
+    getIssuesOfRepository(path).then(result => {
+      setOrganization(result.data.data.organization)
+      setErrors(result.data.errors)
+    })
   }
 
   return (
@@ -60,10 +86,51 @@ const App = () => {
       </form>
 
       <hr />
-
-
+      {organization ? (
+        <Organization organization={organization} errors={errors} />
+      ) : (
+        <p>No information yet...</p>
+      )}
     </div>
   )
 }
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong: </strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    )
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization: </strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+      <Repository repository={organization.repository} />
+    </div>
+  )
+}
+
+const Repository = ({ repository }) => (
+  <div>
+    <p>
+      <strong>In Repository: </strong>
+      <a href={repository.url}>{repository.name}</a>
+    </p>
+
+    <ul>
+      {repository.issues.edges.map(issue => (
+        <li key={issue.node.id}>
+          <a href={issue.node.url}>{issue.node.title}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
 
 export default App
