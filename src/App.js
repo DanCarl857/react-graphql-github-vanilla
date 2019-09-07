0409e1e1527b0124d9b84641737d0500e6676bd8
Daniel Carlson <dancarl758@gmail.com>
2019-09-07 13:01:59 +0100

    Wrote first query to get Github API data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const axiosGithubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
+})
+
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`
+
+const TITLE = "React GraphQL Github Client"
+
+const App = () => {
+
+  const [path, setPath] = useState('the-road-to-learn-react/the-road-to-learn-react')
+
+  useEffect(() => {
+    onFetchFromGithub()
+  }, [])
+
+  const onChange = (event) => {
+    setPath(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  const onFetchFromGithub = () => {
+    axiosGithubGraphQL
+      .post('', { query: GET_ORGANIZATION })
+      .then(result => console.log(result))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{TITLE}</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">
+          Show open issues for https://github.com/
+        </label>
+        <input
+          id="url"
+          type="text"
+          value={path}
+          onChange={onChange}
+          style={{ width: '300px' }}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <hr />
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
